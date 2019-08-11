ff5d4d057e918f3d06281cdfc1f4a984ab566a36
Robert Tyree <tyreer@gmail.com>
2019-08-11 18:39:20 +0100

    add intial pages and adjust eslint sx-a11y/anchor-is-valid

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Index = () => (
+  <Link href="/login">
+    <a>Login</a>
+  </Link>
+);
+
+export default Index;
---
+++ frontend/pages/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Login = () => (
+  <Link href="/">
+    <a>Home</a>
+  </Link>
+);
+
+export default Login;
